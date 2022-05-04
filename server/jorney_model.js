9b713c7f064c425803e02b4405ee014260c01ad2
Viktoria Matveeva <vika030718@gmail.com>
2022-05-04 11:31:25 +0200

    fix

--- server/jorney_model.js
+++ server/jorney_model.js
@@ -3,11 +3,19 @@
 // INSERT INTO jorney (title, start_date, end_date, description, creation_date) VALUES ('Cyprus', '2022-02-03', '2022-02-12', 'Jorney to Cyprus', '1646986027005');
 
 const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "admin",
+//   host: "localhost",
+//   database: "my_jorneys",
+//   password: "root",
+//   port: 5432,
+// });
+
 const pool = new Pool({
-  user: "admin",
-  host: "localhost",
-  database: "my_jorneys",
-  password: "root",
+  user: "jkjjtqyzqxoith",
+  host: "ec2-35-168-194-15.compute-1.amazonaws.com",
+  database: "d3gq3dnm7l2rkp",
+  password: "37d8d42af3ba7e0a67be526bbb810850409387ee56bb867a6b75bf2d93026149s",
   port: 5432,
 });
 
