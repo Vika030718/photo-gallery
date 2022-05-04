748fcbe62afd2db54925e5b32a8445a349d27f56
Viktoria Matveeva <vika030718@gmail.com>
2022-05-04 10:05:35 +0200

    add scheme

--- db_ scheme.sql
+++ db_ scheme.sql
@@ -1,7 +1,1 @@
-CREATE TABLE jorney
-(id SERIAL PRIMARY KEY,
-title TEXT,
-start_date DATE,
-end_date DATE,
-description TEXT,
-creation_date NUMERIC);
+CREATE TABLE JORNEY(id SERIAL PRIMARY KEY, title TEXT, start_date DATE, end_date DATE, description TEXT, creation_date NUMERIC);
