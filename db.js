4a51d26af3abadc9de5a2c2aa7e3f3b19768d259
Viktoria Matveeva <vika030718@gmail.com>
2022-05-06 17:58:10 +0200

    fix db connection

--- db.js
+++ db.js
@@ -1,16 +1,19 @@
-const isProduction = process.env.NODE_ENV === "production";
-if (!isProduction) require("dotenv").config();
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-console.log(isProduction);
-console.log(process.env.DATABASE_URL);
 const { Pool } = require("pg");
+let isProduction = false;
+let ssl = false;
+
+if (process.env.NODE_ENV === "production") {
+  isProduction = true;
+  ssl = { rejectUnauthorized: false };
+} else {
+  require("dotenv").config();
+}
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?`;
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  //   ssl: isProduction,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: ssl,
 });
 
 module.exports = pool;
